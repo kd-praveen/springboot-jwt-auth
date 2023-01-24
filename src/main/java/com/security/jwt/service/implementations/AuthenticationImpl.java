99172f0117750e87b3b70a7af0ed33c50a5be66f
Praveen K D <praveen@wizroots.com>
2023-01-24 18:17:57 +0530

    Refresh token end point implemented

--- src/main/java/com/security/jwt/service/implementations/AuthenticationImpl.java
+++ src/main/java/com/security/jwt/service/implementations/AuthenticationImpl.java
@@ -1,14 +1,20 @@
 package com.security.jwt.service.implementations;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.tomcat.util.json.TokenMgrError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 
 import com.security.jwt.config.JwtService;
@@ -37,6 +43,8 @@ public class AuthenticationImpl implements AuthenticationService{
     private final JwtService jwtService;
 
     private final AuthenticationManager authenticationManager;
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     public RegisterResponseDto register(RegisterRequestDto request) {
@@ -86,13 +94,41 @@ public class AuthenticationImpl implements AuthenticationService{
 
     @Override
     public AuthenticationResponseDto refreshToken(HttpServletRequest request) {
-        final String authHeader = request.getHeader("Authorization");
-        // var expirationTime = jwtService.extractExpireTime(authHeader);
+        String authHeader = request.getHeader("Authorization");
+        String refreshToken = "";
+        String jwtToken = "";
+        Date expirationTime = new Date();
 
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("Invalid JWT signature");
+        }
+
+        refreshToken = authHeader.substring(7);
+
+        final Object tokenType = jwtService.extractTokenType(refreshToken);
+
+        if(tokenType.equals("refresh")){
+            String userEmail = jwtService.extractUsername(refreshToken);
+
+            if (userEmail != null) {
+
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+    
+                if (jwtService.isTokenValid(refreshToken, userDetails)) {
+                // generate jwt tokens
+                jwtToken = jwtService.generateToken(userDetails);
+                expirationTime = jwtService.extractExpireTime(jwtToken);
+                    
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid JWT signature");
+        }
+        
         return AuthenticationResponseDto.builder()
-                .token(authHeader)
-                .refreshToken(authHeader)
-                // .expires_in(expirationTime)
+                .token(jwtToken)
+                .refreshToken(refreshToken)
+                .expires_in(expirationTime)
                 .build();
     }
     
