1c3d3236c84a632cc6a1a134af6a8d2f7629f12c
Praveen K D <praveen@wizroots.com>
2023-03-16 16:49:27 +0530

    Switched SecurityFilterChain configuration syntax to Spring Security Lambda DSL

--- src/main/java/com/security/jwt/config/SecurityConfiguration.java
+++ src/main/java/com/security/jwt/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -28,20 +29,13 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf()
-            .disable()
-            .exceptionHandling()
-            .authenticationEntryPoint(authEntryPoint)
-            .and()
-            .authorizeHttpRequests()
-            .requestMatchers("/api/v1/auth/**")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
+            .csrf(AbstractHttpConfigurer::disable)
+            .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint))
+            .authorizeHttpRequests(authorize -> {
+                authorize.requestMatchers("/api/v1/auth/**").permitAll();
+                authorize.anyRequest().authenticated();
+            })
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authenticationProvider(authenticationProvider)
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
